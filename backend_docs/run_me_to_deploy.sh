db7d2b036c25fbb41106931778bea7edf76f1be4
Jingjia Peng <jingjia@umich.edu>
2025-07-30 17:13:59 +0800

    doc: migrate frontend and backend doc to central doc endpoint

--- backend_docs/run_me_to_deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git remote add backend_doc git@github.com:JI-DeepSleep/DocuSnap-Backend.git
-mkdocs gh-deploy  --force --remote-branch gh-pages --remote-name backend_doc
--- frontend_docs/run_me_to_deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git remote add frontend_doc git@github.com:JI-DeepSleep/DocuSnap-Frontend.git
-mkdocs gh-deploy  --force --remote-branch gh-pages --remote-name frontend_doc
